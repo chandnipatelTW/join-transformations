7a27f5bca731935a4223883dcc9a0e35555310b9
Darshan Jani <darshanj@thoughtworks.com>
2019-04-18 21:19:14 +0530

    added some more joins

---
+++ src/test/scala/main/JoinTest.scala
@@ -0,0 +1,90 @@
+package main
+
+import org.apache.spark.sql.{DataFrame, QueryTest}
+import org.apache.spark.sql.test.SharedSQLContext
+
+class JoinTest extends QueryTest with SharedSQLContext with TestHelpers {
+
+  import testImplicits._
+
+  object TestData {
+    def customers = Seq(
+      (1, "Alex", "M"),
+      (2, "Bill", "M"),
+      (3, "Steffi", "F"),
+      (4, "Mary", "F"),
+      (5, "Julia", "F")
+    ).toDF("customerId", "name", "genderCode")
+
+    def orders = Seq(
+      (1, 1, 5.0),
+      (2, 2, 10.0),
+      (3, 2, 20.0),
+      (4, 2, 30.0),
+      (5, 2, 40.0),
+      (6, 7, 50.0)
+    ).toDF("orderId", "customerId", "amount")
+
+    def genders = Seq(
+      ("M", "Male"),
+      ("F", "Female"),
+      ("O", "Other")
+    ).toDF("genderCode", "gender")
+  }
+
+  test("orders with customer names and genders") {
+
+    val actualDF = TestData.customers
+      .join(TestData.orders, Seq("customerId"))
+      .join(TestData.genders, Seq("genderCode"))
+
+    val expectedDF = Seq(
+      ("M", 1, "Alex", 1, 5.0, "Male"),
+      ("M", 2, "Bill", 2, 10.0, "Male"),
+      ("M", 2, "Bill", 3, 20.0, "Male"),
+      ("M", 2, "Bill", 4, 30.0, "Male"),
+      ("M", 2, "Bill", 5, 40.0, "Male")
+    ).toDF("genderCode", "customerId", "name", "orderId", "amount", "gender")
+
+    checkAnswer(actualDF, expectedDF)
+  }
+
+  test("all unique customers with minimum one order") {
+
+    val actualDF = TestData.customers
+      .join(TestData.orders, Seq("customerId"), "leftsemi")
+
+    val expectedDF = Seq(
+      (1, "Alex", "M"),
+      (2, "Bill", "M")
+    ).toDF("customerId", "name", "genderCode")
+
+    checkAnswer(actualDF, expectedDF)
+  }
+
+  test("all possible customer and orders combinations") {
+
+    val actualDF = TestData.customers
+      .crossJoin(TestData.genders)
+      .drop("genderCode")
+
+    val expectedDF = Seq((1,"Alex","Female"),
+    (1,"Alex","Male"),
+    (1,"Alex","Other"),
+    (2,"Bill","Female"),
+    (2,"Bill","Male"),
+    (2,"Bill","Other"),
+    (3,"Steffi","Female" ),
+    (3,"Steffi","Male"),
+    (3,"Steffi","Other"),
+    (4,"Mary","Female"),
+    (4,"Mary","Male"),
+    (4,"Mary","Other"),
+    (5,"Julia","Female"),
+    (5,"Julia","Male"),
+    (5,"Julia","Other")).toDF("customerId", "name", "gender")
+
+    checkAnswer(actualDF, expectedDF)
+  }
+
+}
